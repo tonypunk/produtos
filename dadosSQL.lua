74f20c22c73cde97f75c65c16ab0b8756a1e9b28
Tony Punk <93947872+tonypunk@users.noreply.github.com>
2022-09-03 11:28:29 -0300

    Update dadosSQL.lua

--- dadosSQL.lua
+++ dadosSQL.lua
@@ -1,1 +1,1 @@
-[ { "host": "135.148.56.168", "db": "punkstudio_protecao", "pw": "ogD6Lc4ZtAzLgAxC", "user": "punkstudios_protecao" } ]
+[ { "host": "135.148.56.168", "db": "punkstudios_prot", "pw": "ogD6Lc4ZtAzLgAxC", "user": "punkstudios_protecao" } ]
