bbbce7eb2c33c2a6d64d3898acc8a250d3cad1e1
Tony Punk <93947872+tonypunk@users.noreply.github.com>
2022-12-17 06:40:33 -0300

    Add files via upload

---
+++ PUNKEscadas/config.lua
@@ -0,0 +1,127 @@
+-----------Configurações-------------
+
+--[[
+; start of San Fierro Airport
+
+bombeiros 278
+swat 285
+
+]]
+
+-----------------------Inicialização-------------------------
+--Descrição: Variaveis de inicialização, não modifique
+sim = true --não tocar
+nao = false	--não tocar
+-------------------------------------------------------------
+
+escadas = {
+	--Los Santos
+	{ -- Prédio perto do contorno norte
+		objeto = sim, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,0}, --rotação da escada
+		inferior = {585.450, -1489.5, 15.406}, --posição inferior da escada
+		superior = {585.450, -1489.5, 73.856}, --posição superior da escada
+	},
+	--
+	{ --Caixa d'agua do estúdio de filmagens
+		objeto = sim, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,180}, --rotação da escada
+		inferior = {896.548, -1251.411, 16.019}, --posição inferior da escada
+		superior = {896.548, -1251.411, 45.225}, --posição superior da escada
+	},
+	--
+	{ --Poste na avenida principal
+		objeto = sim, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,0}, --rotação da escada
+		inferior = {1305.090, -1703.25, 13.547}, --posição inferior da escada
+		superior = {1305.180, -1702.961, 23}, --posição superior da escada
+	},
+	--
+	
+	--San Fierro
+	{ --DP de San Fierro
+		objeto = sim, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,0}, --rotação da escada
+		inferior = {-1641.393, 686.5, 7.664}, --posição inferior da escada
+		superior = {-1641.393, 686.5, 49.664}, --posição superior da escada
+	},
+	--
+	{--Prédio em construção proximo do posto abandonado em San Fierro
+		objeto = sim, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,0}, --rotação da escada
+		inferior = {-2076.429, 336.7, 35.172}, --posição inferior da escada
+		superior = {-2076.429, 336.7, 67.172}, --posição superior da escada
+	},
+	--
+	--Aeroporto San Fierro
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,0}, --rotação da escada
+		inferior = {-1361.05, -696.8, 1.9}, --posição inferior da escada
+		superior = {-1361.05, -696.8, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,0}, --rotação da escada
+		inferior = {-1603.3967 , -696.8, 1.9}, --posição inferior da escada
+		superior = {-1603.425, -696.8, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,58}, --rotação da escada
+		inferior = {-1154.1812, -476.7963, 1.9}, --posição inferior da escada
+		superior = {-1154.1812, -476.7963, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,110}, --rotação da escada
+		inferior = {-1081.7765, -207.9118, 1.9609}, --posição inferior da escada
+		superior = {-1081.7765, -207.9118, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,134.4}, --rotação da escada
+		inferior = {-1182.5297, 60.4368, 1.9609}, --posição inferior da escada
+		superior = {-1182.5297, 60.4368, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,45}, --rotação da escada
+		inferior = {-1115.7112, 335.1707, 1.9609}, --posição inferior da escada
+		superior = {-1115.7112, 335.1707, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,220}, --rotação da escada
+		inferior = {-1164.7069, 370.1683, 1.9609}, --posição inferior da escada
+		superior = {-1164.7069, 370.1683, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,225}, --rotação da escada
+		inferior = {-1444.5359, 90.3538, 1.9609}, --posição inferior da escada
+		superior = {-1444.5359, 90.3538, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,225}, --rotação da escada
+		inferior = {-1618.7654, -83.8916, 1.9609}, --posição inferior da escada
+		superior = {-1618.7654, -83.8916, 13.5}, --posição superior da escada
+	},
+	--
+	{
+		objeto = nao, --ativar/desativar criação do objeto escada
+		rotacao = {0,0,270}, --rotação da escada
+		inferior = {-1736.5709, -445.9409, 1.9609}, --posição inferior da escada
+		superior = {-1736.5709, -445.9409, 13.5}, --posição superior da escada
+	},
+	--
+}
