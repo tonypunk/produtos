869dea73a65ebb38cb39cab4f58b7a63943788b3
Tony Punk <93947872+tonypunk@users.noreply.github.com>
2023-11-28 13:38:51 -0300

    Add files via upload

--- PUNKClimas/config.lua
+++ PUNKClimas/config.lua
@@ -8,10 +8,10 @@ hora = sim --ativar/desativar hora real
 ciclo = 3 -- tempo de duração dos climas em minutos
 
 climas = {			--tabela de climas modificável, já vem configurado nos climas padrões do GTA, para mais informações: https://wiki.multitheftauto.com/wiki/Weather
-	["Los Santos"] = {0,1,2,3,4}, -- Climas de Los Santos
-	["San Fierro"] = {0}, -- Climas de San Fierro
-	["Las Venturas"] = {10,11,12}, -- Climas de Las Venturas
-	["Red County"] = {13,14,15,16}, -- Climas de Red County
-	["Bone County"] = {17,18,19}, -- Climas de Bone County
+	["Los Santos"] = {0}, -- Climas de Los Santos
+	["San Fierro"] = {8}, -- Climas de San Fierro
+	["Las Venturas"] = {0}, -- Climas de Las Venturas
+	["Red County"] = {0}, -- Climas de Red County
+	["Bone County"] = {0}, -- Climas de Bone County
 }
 --------------------------------------------------------
