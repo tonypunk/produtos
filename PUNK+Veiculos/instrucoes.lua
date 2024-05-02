c891379e30ca893b4db5d53ba4b77ccd7d57aff3
Tony Punk <tonytstreetpunk@hotmail.com>
2024-05-01 22:30:29 -0300

    att

--- PUNK+Veiculos/instrucoes.lua
+++ PUNK+Veiculos/instrucoes.lua
@@ -18,27 +18,55 @@ _createVehicle = createVehicle --não tocar
 _getElementModel = getElementModel --não tocar
 _setElementModel = setElementModel --não tocar
 
-function createVehicle (...)
+function isNativeVehicle (id)
 	for i=400, 611 do
 		if arg[1] == i then
-		return _createVehicle (unpack (arg))
+		return true
 		end
 	end
+end
+
+_createVehicle = createVehicle
+function createVehicle (...)
+	if isNativeVehicle (arg[1]) then return _createVehicle (unpack (arg)) end
 return exports["PUNK+Veiculos"]:createVehicle (unpack (arg))
 end
 Vehicle = createVehicle
 
+_getElementModel = getElementModel
 function getElementModel (elemento)
 	if getElementType (elemento) == "vehicle" then return exports["PUNK+Veiculos"]:getVehicleModel (elemento) end
 return _getElementModel (elemento)
 end
 
+_setElementModel = setElementModel
 function setElementModel (...)
-	for i=400, 611 do
-		if arg[2] == i then
-		return _setElementModel (unpack(arg))
-		end
-	end
+	if isNativeVehicle (arg[1]) then return _setElementModel (unpack (arg)) end
 return exports["PUNK+Veiculos"]:setVehicleModel (unpack(arg))
 end
+
+_engineImportTXD = engineImportTXD
+function engineImportTXD (...)
+	if isNativeVehicle (arg[1]) then return _engineImportTXD (unpack (arg)) end
+return exports["PUNK+Veiculos"]:engineImportVehicleTXD (unpack(arg))
+end
+
+_engineReplaceModel = engineReplaceModel
+function engineReplaceModel (...)
+	if isNativeVehicle (arg[1]) then return _engineReplaceModel (unpack (arg)) end
+return exports["PUNK+Veiculos"]:engineReplaceVehicleModel (unpack(arg))
+end
+
+_engineFreeModel = engineFreeModel
+function engineFreeModel (...)
+	if isNativeVehicle (arg[1]) then return _engineFreeModel (unpack (arg)) end
+return exports["PUNK+Veiculos"]:engineFreeVehicleModel (unpack(arg))
+end
+
+_engineRestoreModel = engineRestoreModel
+function engineRestoreModel (...)
+	if isNativeVehicle (arg[1]) then return _engineRestoreModel (unpack (arg)) end
+return exports["PUNK+Veiculos"]:engineRestoreVehicleModel (unpack(arg))
+end
+
 ]]
